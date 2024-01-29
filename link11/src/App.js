06173802d76bcfffa59ded5fc77c541320e9b752
Sneha Khan <96278413+snehakha@users.noreply.github.com>
2024-01-30 05:21:07 +0530

    Add files via upload

---
+++ link11/src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="body"> 
+            <p className='header'>
+              <u><h2>Welcome to our Videos section</h2></u>
+            </p>
+            <p className="intro">
+                Welcome to the videos section. Here we have kept list of 
+                some intresting videos on climate change for both adults
+                and children alike. These videos demostrate the problems
+                and solutions that are being developed for climate change.
+                These will give us a idea about the gravity of the situation
+                in which are currently in.
+              </p>
+
+                <ul>
+                    <li><a href="https://youtu.be/G9t__9Tmwv4?si=ewkvTNuw0veIAxcD" target='_blank' rel='noopener noreferrer'>Video 1</a></li>
+                    <li><a href="https://youtu.be/EuwMB1Dal-4?si=nxCjRTD7ZRowbMYa" target='_blank' rel='noopener noreferrer'>Video 2</a></li>
+                    <li><a href="https://youtu.be/vrXweCjRi9w?si=_48mMEMZnWW-7sbz" target='_blank' rel='noopener noreferrer'>Video 3</a></li>
+                    <li><a href="https://youtube.com/watch?v=SN5-DnOHQmE&feature=shared" target='_blank' rel='noopener noreferrer'>Video 4</a></li>
+                    <li><a href="https://youtu.be/sDS1sFg6lNw?si=VvObzdMnRQ4_w0mc" target='_blank' rel='noopener noreferrer'>Video 5</a></li>
+                    <li><a href="https://youtu.be/G4H1N_yXBiA?si=1arXdcCtxl_GHZiG" target='_blank' rel='noopener noreferrer'>Video 6</a></li>
+                    <li><a href="https://youtu.be/myZAvqqp9Jc?si=unr0tWpNdoU-bSLD" target='_blank' rel='noopener noreferrer'>Video 7</a></li>
+                    <li><a href="https://youtu.be/3CM_KkDuzGQ?si=Ml6kBQyc2-TSCY1I" target='_blank' rel='noopener noreferrer'>Video 8</a></li>
+                    <li><a href="https://youtube.com/watch?v=Hy51gQxhAGE&feature=shared" target='_blank' rel='noopener noreferrer'>Video 9</a></li>
+                    <li><a href="https://youtu.be/0F3QPY83NZQ?si=ay1lpTigBWRDwYnf" target='_blank' rel='noopener noreferrer'>Video 10</a></li>
+                    <li><a href="https://youtu.be/jnFeXaL02Fg?si=VNjpYWO2_bd0hB3G" target='_blank' rel='noopener noreferrer'>Video 11</a></li>
+                    <li><a href="https://youtu.be/dcBXmj1nMTQ?si=EgGh4GPfahnB6u_x" target='_blank' rel='noopener noreferrer'>Video 12</a></li>
+                    <li><a href="https://youtu.be/vjZnUl3OkCI?si=NJ7Cf-CQsxgMk8vS" target='_blank' rel='noopener noreferrer'>Video 13</a></li>
+                    <li><a href="https://youtu.be/Kb3Tnmwx7Rc?si=BZ7HTAylRIEmIeQH" target='_blank' rel='noopener noreferrer'>Video 14</a></li>
+                    <li><a href="https://youtu.be/9PFhrpyWV-w?si=cLptVjfiAAkU91ko" target='_blank' rel='noopener noreferrer'>Video 15</a></li>
+                </ul>
+
+                <p className='documents'>
+                  Additionally, we have some latest news and documents to be presented in this topic.
+                </p>
+
+                <ul>
+                  <li><a href='https://unfccc.int/sites/default/files/resource/14Nov2023%20Dominican%20Republic%20Carbon%20Market%20Simulation%20Exercise%20Final%20report.pdf'>Doc 1</a></li>
+                  <li><a href='https://unfccc.int/sites/default/files/resource/CNNow_Report_MANTENIMIENTOS_Y_SERVICIOS_TECMAN_2022.pdf'>Doc 2</a></li>
+                  <li><a href='https://static.pib.gov.in/WriteReadData/specificdocs/documents/2021/dec/doc202112101.pdf'>Doc 3</a></li>
+                  <li><a href='https://news.un.org/en/news/topic/climate-change'>News article 1</a></li>
+                  <li><a href='https://climate.nasa.gov/news/3282/nasa-announces-summer-2023-hottest-on-record/'>News article 2</a></li>
+                  <li><a href='https://economictimes.indiatimes.com/news/economy/indicators/climate-change-may-cut-labour-productivity-in-india-to-40-by-2100-study/articleshow/107010818.cms'>News article 3</a></li>
+                  <li><a href='https://unfccc.int/news/dubai-youth-climate-dialogue-during-cop28-highlights-young-peoples-priorities-for-climate-action'>News article 4</a></li>
+                  <li><a href='https://unfccc.int/news/how-climate-technology-is-being-ramped-up'>News article 5</a></li>
+                </ul>
+
+                <button className='home-htn'><a href='https://rohammaiti.github.io/Earth_guardians/'>Go back home</a></button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
